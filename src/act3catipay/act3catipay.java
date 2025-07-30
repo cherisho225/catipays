102cb2d6735b8ebd06a8a43aad95bf9563edcaaa
Musume-x <rjett131@gmail.com>
2025-07-30 14:38:16 +0800

    added act3

---
+++ src/act3catipay/act3catipay.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package act3catipay;
+
+/**
+ *
+ * @author PC 41
+ */
+public class act3catipay {
+    public static void main(String[] args) {
+        System.out.println("HEllo World");
+    }
+    
+}
