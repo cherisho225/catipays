97bbdee25f46eff725d4aa7829c970f1616da612
cherisho225 <catipaycherish61@gmail.com>
2025-07-29 23:24:39 -0700

    added act 2

---
+++ src/cheactivity2/cheactivity2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cheactivity2;
+
+import java.util.Scanner; 
+
+public class cheactivity2 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter employee name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter hourly rate: ");
+        float rate = sc.nextFloat();
+        System.out.println("Enter total hours worked: ");
+        float hours = sc.nextFloat();
+        
+        System.out.println("-----------Wage Summary-----------");
+        System.out.println("Employee: "+name);
+        System.out.println("Hourly rate: P"+rate);
+        System.out.println("Hours worked: "+hours);
+        System.out.println("Gross weekly wage: "+(hours*rate));
+        System.out.printf("SSS Contribution(10%%): P%.2f)\n", (hours*rate)*0.1);
+        System.out.println("--------------------------------");
+        System.out.printf("NEt weekly Wage: P%.2f\n\n",+((hours*rate)-((hours*rate)*0.1)));
+        
+        
+              
+    }
+    
+}
